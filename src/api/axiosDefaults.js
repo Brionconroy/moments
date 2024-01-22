71e0cf23e0a8f0613dc06a291fa5d790b124c6c1
Brionconroy <razvanbrion@gmail.com>
2024-01-22 11:30:13 +0000

    add signin singup

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api-2023-6c240fdbe670.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
